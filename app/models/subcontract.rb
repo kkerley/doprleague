8587329332b5b209f17d4cf0ca38307f4b8d6d44
Kyle Kerley <kyle.l.kerley@gmail.com>
2014-07-31 21:11:38 -0600

    Current year update

--- app/models/subcontract.rb
+++ app/models/subcontract.rb
@@ -16,7 +16,7 @@ class Subcontract < ActiveRecord::Base
   def self.current_year
     # The new seasons starts on 8/1 so technically, it's the previous year through 7/31
     current_date = Time.now
-    if current_date.month < 8
+    if current_date.month < 9
       current_date.year - 1
     else
       current_date.year
